ac2a496f7d28f9be0f32a23a7b5f6a58cb357c9d
Gwen Larson <glarson284@gmail.com>
2015-07-07 18:14:30 -0400

    added post_id to comment model

---
+++ db/migrate/20150707221153_add_comment_column.rb
@@ -0,0 +1,5 @@
+class AddCommentColumn < ActiveRecord::Migration
+  def change
+     add_column :comments, :post_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150707200847) do
+ActiveRecord::Schema.define(version: 20150707221153) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text    "comment"
+    t.integer "user_id", null: false
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text     "body"
