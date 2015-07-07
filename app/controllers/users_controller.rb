d46e4d0b92266aaff88599b6de67e116eeace646
Ognian <ogidan@abv.bg>
2015-07-07 17:44:40 -0400

    we can edit and destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,19 @@ class UsersController < ApplicationController
     @user = User.find params[:id]
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes params[:user]
+    flash[:alert] = "Just updated #{@user.fname}"
+    redirect_to user_path(@user)
+  end
+
+  def destroy
+    @user = User.find params[:id]
+    @user.destroy
+    flash[:alert] = "Just deleted #{@user.fname}"
+    redirect_to users_path
+  end
   
 
 
